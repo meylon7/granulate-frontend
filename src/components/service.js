4b1cb109ccabd7a359910c30a42c0b59adca701c
moshe eylon <54767002+meylon7@users.noreply.github.com>
2021-03-11 20:59:21 +0200

    Add files via upload

--- src/components/service.js
+++ src/components/service.js
@@ -1,10 +1,9 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import useSessionStorage from '../hook/useSessionStorage'
 const MAIN_AREA = {
     margin: '0 auto',
     textAlign:'center',
     maxHeight:'400px',
-    textAlign: 'center',
     left:'50%',
     position: 'fixed',
     top: '50%',
@@ -16,7 +15,7 @@ const Service = () => {
     return (
         <div style={MAIN_AREA}>
                 {
-                    serviceStorage.map(item => <h2 className='title-effect short-text' key={item.key}>{item.key} : {item.value}</h2>)
+                    serviceStorage.map(item => <h2 className='title-effect short-text' key={item.key}>{item.key} : {item.value}</h2>) 
                 }
         </div>
     )
